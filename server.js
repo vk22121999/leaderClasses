e5bbcb2738e0c0d56c310b54e0ef7d30e3165ef0
vk22121999 <avkish99@gmail.com>
2020-09-16 16:40:49 +0530

    version1

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(favicon(__dirname + '/build/favicon.ico'));
+// app.use(favicon(__dirname + '/build/favicon.ico'));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
